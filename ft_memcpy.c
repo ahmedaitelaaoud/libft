5df7caa7236547f094332f8288c71ddac3fce7a3
Ahmed Ait Elaaoud <aait-ela@c1r6p10.1337.ma>
2025-10-30 17:41:05 +0100

    hjadsgjfgasjdgfa

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -14,12 +14,12 @@
 
 void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
-	unsigned char		*d;
-	const unsigned char	*s;
-	size_t				i;
+	unsigned char	*d;
+	unsigned char	*s;
+	size_t			i;
 
 	d = (unsigned char *)dest;
-	s = (const unsigned char *)src;
+	s = (unsigned char *)src;
 	i = 0;
 	while (i < n)
 	{
@@ -28,11 +28,3 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	}
 	return (dest);
 }
-/*#include <stdio.h>
-int	main(void)
-{
-	char *src = "Ahmed";
-	char dest[3];
-	ft_memcpy(dest, src, 3);
-	printf("%s\n", dest);
-}*/
