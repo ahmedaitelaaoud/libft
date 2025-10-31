7feb71e87283193d66ce87ab50fc02f83918d58b
Ahmed Ait Elaaoud <aait-ela@c1r4p12.1337.ma>
2025-10-31 19:39:29 +0100

    ready to push

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: aait-ela <aait-ela@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/18 12:47:51 by aait-ela          #+#    #+#             */
-/*   Updated: 2025/10/29 11:37:45 by aait-ela         ###   ########.fr       */
+/*   Updated: 2025/10/31 19:38:40 by aait-ela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 	size_t	src_len;
 	size_t	i;
 
+	if (!src || !dst)
+		return (0);
 	src_len = ft_strlen(src);
 	if (size == 0)
 		return (src_len);
@@ -29,14 +31,3 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 	dst[i] = '\0';
 	return (src_len);
 }
-/*#include <stdio.h>
-
-int	main(void)
-{
-	char src[] = "ABCDEFG";
-	char dst[4];
-	size_t ret = ft_strlcpy(dst, src, 0);
-	printf("%s\n", src);
-	printf("%ld\n", ret);
-	// ret == 7, dst == "ABC"
-}*/
