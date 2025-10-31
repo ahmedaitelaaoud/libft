a092734207812c1f9b8db925852675f860d19576
Ahmed Ait Elaaoud <aait-ela@c2r1p11.1337.ma>
2025-10-31 23:28:48 +0100

    ready to push

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -16,7 +16,7 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	size_t	i;
 
-	if (!s1 && !s2)
+	if (!s1 || !s2)
 	{
 		return (0);
 	}
@@ -31,11 +31,3 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	}
 	return (0);
 }
-/*#include <stdio.h>
-int	main(void)
-{
-	char s1[] = "ahmed";
-	char s2[] = "ahmedd      ";
-	int a = ft_strncmp(s1, s2, 7);
-	printf("%d\n", a);
-}*/
