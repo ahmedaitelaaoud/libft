d95c1c1c2716cefd34937bc42d2be2d70e2bbbec
Ahmed Ait Elaaoud <aait-ela@c1r4p12.1337.ma>
2025-10-31 18:57:19 +0100

    ready to push

--- ft_memset.c
+++ ft_memset.c
@@ -30,31 +30,3 @@ void	*ft_memset(void *s, int c, size_t n)
 	}
 	return (s);
 }
-// #include <stdio.h>
-// int main()
-// {
-// 	char *a = ft_memset(NULL, 5, 2);
-// 	printf("1");
-// }
-/*#include <stdio.h>
-
-int	main(void)
-{
-	unsigned char tt = 255;
-	tt = tt +1;
-	printf("%c\n",tt);
-	/*char	str[] = "hygugugjhg";
-	char	*move;
-	int		i;
-	//*(move + 1);
-
-	// printf("%d\n", b);
-	move = malloc(sizeof(str)+1);
-	i = 0;
-	while (i < sizeof(str))
-	{
-		ft_memset(move + i, str[i], 1);
-		i++;
-	}
-	printf("%s\n", move);/*
-}*/
