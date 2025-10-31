2c9e5e925b427c06489f99c3013370fe5b94cfed
Ahmed Ait Elaaoud <aait-ela@c1r4p12.1337.ma>
2025-10-31 17:15:06 +0100

    ready to push

--- ft_isalpha.c
+++ ft_isalpha.c
@@ -16,12 +16,3 @@ int	ft_isalpha(int c)
 {
 	return ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'));
 }
-/*
-#include <stdio.h>
-
-int	main(void)
-{
-	printf("%d\n", ft_isalpha(65));
-	printf("%d", ft_isalpha(1));
-	return (0);
-}*/
