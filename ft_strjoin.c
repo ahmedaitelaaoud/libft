e198b5ae974e5b7d61255c76aa4ab703b91c74d5
Ahmed Ait Elaaoud <aait-ela@c2r8p14.1337.ma>
2025-11-01 16:43:45 +0100

    ready to push

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -17,6 +17,7 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	size_t	len;
 	char	*new;
 	size_t	i;
+	size_t	j;
 
 	i = 0;
 	len = ft_strlen(s1) + ft_strlen(s2);
@@ -24,18 +25,18 @@ char	*ft_strjoin(char const *s1, char const *s2)
 		return (NULL);
 	if (!(new = malloc(len + 1)))
 		return (NULL);
-	while (s1[i] != '\0')
-		*(new ++) = s1[i++];
-	i = 0;
-	while (s2[i] != '\0')
-		*(new ++) = s2[i++];
+	while (s1[i])
+	{
+		new[i] = s1[i];
+		i++;
+	}
+	j = 0;
+	while (s2[j] != '\0')
+	{
+		new[i] = s2[j];
+		i++;
+		j++;
+	}
 	new[i] = '\0';
-	new -= len;
 	return (new);
 }
-// #include <stdio.h>
-
-// int	main(void)
-// {
-// 	printf("%s \n", ft_strjoin("Ahmeds", " + Walid"));
-// }
