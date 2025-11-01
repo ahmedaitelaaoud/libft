2d129eaabf6ae5e5267643ffb42da6b1d10c6e3c
Ahmed Ait Elaaoud <aait-ela@c2r8p14.1337.ma>
2025-11-01 16:52:00 +0100

    ready to push

--- ft_substr.c
+++ ft_substr.c
@@ -34,10 +34,3 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	new[i] = '\0';
 	return (new);
 }
-// #include <stdio.h>
-
-// int	main(void)
-// {
-// 	printf("%s \n", ft_substr("Bonjour comment ca va?", 3, 100));
-
-// }
